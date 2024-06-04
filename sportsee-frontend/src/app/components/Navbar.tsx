44eb0c8fdb34da5c1bfae4fd8146c8246a70e197
Souleymane7800 <souleymane.doumbia7800@gmail.com>
2024-06-04 12:54:22 +0200

    navbar

---
+++ sportsee-frontend/src/app/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function Navbar() {
+      return (
+            <div className='h-[91px] bg-[#020203] max-w-[1440px] mx-auto text-white dropshadow1 flex items-center justify-between '>
+                  <div className='pl-[28px]'>
+                        <Image
+                              src='/logo_sportsee.png'
+                              width={178}
+                              height={61}
+                              alt='Logo SportSee'
+                        />
+                  </div>
+                  <div className=''>
+                        <nav className='font-medium '>
+                              <ul className='flex text-2xl lg:gap-[219px] pr-[41px] font-medium gap-[50px]'>
+                                    <li>Accueil</li>
+                                    <li>Profil</li>
+                                    <li>Réglage</li>
+                                    <li>Communauté</li>
+                              </ul>
+                        </nav>
+                  </div>
+            </div>
+      );
+}
--- sportsee-frontend/src/app/layout.tsx
+++ sportsee-frontend/src/app/layout.tsx
@@ -1,56 +1,55 @@
-import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
 
 const roboto = Roboto({
-      weight: ['400', '500', '700'],
-      subsets: ['latin'],
-      display: 'swap',
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-      title: 'SportSee-frontend',
-      description: 'Sport Analytics Dashboard',
+  title: "SportSee-frontend",
+  description: "Sport Analytics Dashboard",
 };
 
 // const faviconLinks = [
-//       {
-//             rel: 'apple-touch-icon',
-//             sizes: '180x180',
-//             href: '/apple-touch-icon.png',
-//       },
-//       {
-//             rel: 'icon',
-//             type: 'image/png',
-//             sizes: '32x32',
-//             href: '/favicon-32x32.png',
-//       },
-//       {
-//             rel: 'icon',
-//             type: 'image/png',
-//             sizes: '16x16',
-//             href: '/favicon-16x16.png',
-//       },
+//   {
+//     rel: "apple-touch-icon",
+//     sizes: "180x180",
+//     href: "/apple-touch-icon.png",
+//   },
+//   {
+//     rel: "icon",
+//     type: "image/png",
+//     sizes: "32x32",
+//     href: "/favicon-32x32.png",
+//   },
+//   {
+//     rel: "icon",
+//     type: "image/png",
+//     sizes: "16x16",
+//     href: "/favicon-16x16.png",
+//   },
 // ];
 
 // const metaTags = [
-//       { name: 'manifest', content: '/site.webmanifest' },
-//       { name: 'msapplication-TileColor', content: '#da532c' },
-//       { name: 'theme-color', content: '#ffffff' },
-//       { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+//   { name: "manifest", content: "/site.webmanifest" },
+//   { name: "msapplication-TileColor", content: "#da532c" },
+//   { name: "theme-color", content: "#ffffff" },
+//   { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
 // ];
 
 export default function RootLayout({
-      children,
+  children,
 }: Readonly<{
-      children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-      return (
-            <>
-                  <html lang='en' className='antialiased'>
-                        <body className={roboto.className}>{children}</body>
-                  </html>
-            </>
-      );
+  return (
+    <>
+      <html lang="en" className="antialiased">
+        <body className={roboto.className}>{children}</body>
+      </html>
+    </>
+  );
 }
-
